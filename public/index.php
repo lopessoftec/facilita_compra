b14368172b8613ffc78c5dde88b54bd30a0ff643
lopessoftec <lopessoftec@gmail.com>
2020-05-25 19:01:44 -0300

    iniciando projeto facilita compra

---
+++ public/index.php
@@ -0,0 +1,11 @@
+use \PlugRoute\PlugRoute;
+use \PlugRoute\RouteContainer;
+use \PlugRoute\Http\RequestCreator;
+
+$route = new PlugRoute(new RouteContainer(), RequestCreator::create());
+
+$route->get('/', function() {
+echo 'basic route';
+});
+
+$route->on();
